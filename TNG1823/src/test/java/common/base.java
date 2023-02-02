aa2692b521f8044bc68d4f429e389c0a6c5e8ce6
areeb <areeb@DESKTOP-4104OKU.fios-router.home>
2023-02-02 15:59:27 -0500

    123

--- TNG1823/src/test/java/common/base.java
+++ TNG1823/src/test/java/common/base.java
@@ -2,18 +2,9 @@ package common;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class base {
-  @Test
-  public void f() {
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
-
-  @AfterTest
-  public void afterTest() {
-  }
-
 }
